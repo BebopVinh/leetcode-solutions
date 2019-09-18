cc166588bacf45abff508eeab165009e87adfac2
BebopVinh <vinhdo07@gmail.com>
2019-09-17 17:48:13 -0700

    add problem 118

---
+++ 0118_pascals_triangle.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+def generate(num_rows)
+    i = 1
+    ans = []
+    prev = nil
+    until i > num_rows
+        row = Array.new(i) {1}
+        if i > 2
+            row = mod_middle(row, prev)
+        end
+        ans << row
+        prev = ans[i - 1]
+        i += 1
+    end
+    ans
+end
+
+def mod_middle(row, prev)
+    j = 1
+    binding.pry
+    until j == row.length
+        row[j] = prev[j] + prev[j - 1]
+        j += 1
+    end
+    row
+    binding.pry
+end
+
+generate(5)
