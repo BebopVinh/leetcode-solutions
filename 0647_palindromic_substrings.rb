90ae272ba2b41339209757682cbeff303512f510
BebopVinh <vinhdo07@gmail.com>
2019-09-27 02:32:55 -0700

    WIP: Problem 0647 - accounting for more cases
    Odd and Even lengths where string is multiple of the same letter.

--- 0647_palindromic_substrings.rb
+++ 0647_palindromic_substrings.rb
@@ -1,15 +1,6 @@
 require 'pry'
 
 def count_substrings(s)
-#     total = 0 + s.size
-#     hash = {}
-#     count = {odd: 0, even: 0}
-#     hash.default = 0
-#     s.each do |c|
-#         hash[c] += 1
-#     end
-    
-#     hash.values.each {|n| (n.odd?)? count[:odd] += 1 : count[:even] += 1}
 
     midpoint, lmid, rmid = 0, 0, 0
     left, right, total = 0, 0, 0
@@ -19,13 +10,17 @@ def count_substrings(s)
         left = midpoint - 1
         right = midpoint + 1
     else
-        rmid = s.length/2
-        lmid = rmid - 1
+        right = s.length/2
+        left = right - 1
     end
+    
+    repeat = false
     
     while s.length.odd? && right < s.length
         total += 1 if s[left] == s[midpoint]
         total += 1 if s[right] == s[midpoint]
+        repeat = (s[left] == s[midpoint] && s[midpoint] == s[right])? true : false
+        total += 4 if right >= midpoint + 2 && repeat == true
         if s[left] != s[right]
             total += 2
         elsif s[left] == s[right]
@@ -35,18 +30,21 @@ def count_substrings(s)
         right += 1
     end
     
-    while s.length.even? && rmid < s.length
+    while s.length.even? && right < s.length
 
-        if s[rmid] != s[lmid]
+        if s[right] != s[left]
             total += 2
-        elsif s[rmid] == s[lmid]
+        elsif s[right] == s[left] && right >= (s.length/2 + 1)
+            total += 5
+        elsif s[right] == s[left]
             total += 3
         end
-        lmid -= 1
-        rmid += 1
+        left -= 1
+        right += 1
     end
     
     total
 end
 
-count_substrings("aa")
+
+puts count_substrings("aaaa")
